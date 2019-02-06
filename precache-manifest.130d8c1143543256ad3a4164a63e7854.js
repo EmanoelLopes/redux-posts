ce2d508a7a57cc0456b576aa6242b106f2c6b9f1
emanoell <emanoel.lopes.web@gmail.com>
2019-02-06 07:51:19 -0200

    Updates

---
+++ precache-manifest.130d8c1143543256ad3a4164a63e7854.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf86b8bf5c0276396d9e",
+    "url": "/redux-posts/static/js/runtime~main.cf86b8bf.js"
+  },
+  {
+    "revision": "b827a50c004a75ced009",
+    "url": "/redux-posts/static/js/main.b827a50c.chunk.js"
+  },
+  {
+    "revision": "aa667713ed878cc5c6e4",
+    "url": "/redux-posts/static/js/1.aa667713.chunk.js"
+  },
+  {
+    "revision": "b827a50c004a75ced009",
+    "url": "/redux-posts/static/css/main.237c7bfe.chunk.css"
+  },
+  {
+    "revision": "693d97521234ff945cdb46fe5617414f",
+    "url": "/redux-posts/index.html"
+  }
+];
